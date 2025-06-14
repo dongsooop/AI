88a7fa4a4bb0504244d934b1775ebfb5bed2e3b8
yu <wptmd97@naver.com>
2025-06-15 03:49:19 +0900

    feat : official schedule insert query implement

--- text_filtering/text_filtering_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from text_filtering.text_filtering import router as text_filter_router
-from text_filtering.text_filtering_rule import router as text_filter_rule_router
-
-app = FastAPI()
-
-# 두 개의 라우터 등록
-app.include_router(text_filter_router)
-app.include_router(text_filter_rule_router)
