7e9bd7e08cc6accb3b46d4cc3c4a172e04c8737e
yu <wptmd97@naver.com>
2025-09-01 15:05:10 +0900

    feat : timetable image file size limit

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from text_filtering.text_filtering import router as text_filter_router
 from text_filtering.text_filtering_rule import router as text_filter_rule_router
 from image_analysis.timetable_analysis import router as timetable
 
 app = FastAPI()
+
+MAX_FILE_SIZE = 3 * 1024 * 1024
+
+@app.middleware("http")
+async def limit_upload_size(request: Request, call_next):
+    content_length = request.headers.get("content-length")
+    if content_length and int(content_length) > MAX_FILE_SIZE:
+        raise HTTPException(status_code=413, detail="File too large")
+
+    return await call_next(request)
 
 # 라우터 등록
 app.include_router(text_filter_router)
